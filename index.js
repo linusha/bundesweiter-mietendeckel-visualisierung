d7735fd0ad4f323fbd645f52479bbd5a59410417
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-11-10 16:25:46 +0100

    All cities with correct coordinates

--- index.js
+++ index.js
@@ -3,10 +3,56 @@
 
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
+// name, lat, rent, market category
+let cities = [
+    {name: "Bochum", long: 7.12,lat:51.48, rent: 15},
+    {name: "Chemnitz", long: 12.92,lat:50.83, rent: 15},
+    {name: "Dortmund", long: 7.46,lat: 51.51, rent: 15},
+    {name: "Duisburg", long: 6.76,lat:51.43, rent: 15},
+    {name: "Erfurt", long: 11.03,lat: 50.97, rent: 15},
+    {name: "Essen", long: 7.01,lat:51.45, rent: 15},
+    {name: "Gelsenkirchen", long: 7.12,lat:51.50, rent: 15},
+    {name: "Hamm", long: 7.82,lat:51.68, rent: 15},
+    {name: "Krefeld", long: 6.58,lat:51.33, rent: 15},
+    {name: "Mönchengladbach", long: 6.44,lat:51.18, rent: 15},
+    {name: "Mühlheim an der Ruhr", long: 8.83,lat:50.11, rent: 15},
+    {name: "Oberhausen", long: 6.88,lat:51.47, rent: 15},
+    {name: "Rostock", long: 12.14,lat:54.08, rent: 15},
+    {name: "Wuppertal", long: 7.16,lat:50.77, rent: 15},
+    {name: "Aachen", long: 6.08,lat:50.77, rent: 15},
+    {name: "Bonn", long: 7.09,lat:50.73, rent: 15},
+    {name: "Braunschweig", long: 10.52,lat:52.26, rent: 15},
+    {name: "Dresden", long: 13.73,lat:51.05, rent: 15},
+    {name: "Düsseldorf", long: 6.77,lat:51.22, rent: 15},
+    {name: "Hannover", long: 9.73,lat:52.37, rent: 15},
+    {name: "Karlsruhe", long: 8.40,lat:49.00, rent: 15},
+    {name: "Kiel", long: 10.13,lat:54.32, rent: 15},
+    {name: "Leipzig", long: 12.37,lat:51.33, rent: 15},
+    {name: "Leverkusen", long: 6.98,lat:51.03, rent: 15},
+    {name: "Lübeck", long: 10.68,lat:53.86, rent: 15},
+    {name: "Ludwigshafen am Rhein", long: 8.44, lat: 49.48, rent: 20},
+    {name: "Mannheim", long: 8.47, lat: 49.49, rent: 40},
+    {name: "Münster", long: 7.62,lat:51.96, rent: 15},
+    {name: "Nürnberg", long: 11.07,lat:49.45, rent: 15},
+    {name: "Potsdam", long: 13.06,lat:52.39, rent: 15},
+    {name: "Wiesbaden", long: 8.24,lat:50.08, rent: 15},
+    {name: "Berlin", long: 13.41,lat:52.52, rent: 15},
+    {name: "Bielefeld", long: 8.53,lat:52.03, rent: 15},
+    {name: "Frankfurt am Main", long: 8.68,lat:50.11, rent: 15},
+    {name: "Freiburg im Breigau", long: 7.85,lat:47.99, rent: 15},
+    {name: "Hamburg", long: 10.01,lat:53.57, rent: 15},
+    {name: "Heidelberg", long: 8.69,lat:49.40, rent: 15},    
+    {name: "Köln", long: 6.95,lat:50.93, rent: 15},
+    {name: "Mainz", long: 8.27,lat:49.98, rent: 15},
+    {name: "München", long: 11.57,lat:48.13, rent: 15},
+    {name: "Osnabrück", long: 8.04,lat:52.27, rent: 15},
+    {name: "Stuttgart", long: 9.17,lat:48.78, rent: 15},
+]
+
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 700 - margin.left - margin.right,
-    height = 900 - margin.top - margin.bottom;
+    height = 400 - margin.top - margin.bottom;
 
 // The svg
 const svg = d3.select("#mapContainer")
@@ -43,15 +89,10 @@ svg.call(d3.zoom().on("zoom", function (event) {
 }))
 
 
-let markers = [
-{name: "berlin", long: 13.24,lat:52.31, rent: 15},
-{name: "munich", long: 11.57, lat: 48.13, rent: 20},
-{name: "freiburg", long: 7.85, lat: 47.99, rent: 40}
-]
 
     // Bars
 let cityBars = svg.selectAll("mybar")
-.data(markers)
+.data(cities)
 .enter()
 .append("rect")
   .attr("width", 10)
