65649cf83d9867acc3714d727fe1185583a50ddb
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-11-10 13:42:05 +0100

    Barchart on map with transition

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 700 - margin.left - margin.right,
+    height = 900 - margin.top - margin.bottom;
+
 // The svg
-const svg = d3.select("svg"),
-width = +svg.attr("width"),
-height = +svg.attr("height");
+const svg = d3.select("#mapContainer")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
 // Map and projection
 const projection = d3.geoMercator()
@@ -35,34 +44,32 @@ svg.call(d3.zoom().on("zoom", function (event) {
 
 
 let markers = [
-{name: "berlin", long: 13,lat:52},
+{name: "berlin", long: 13.24,lat:52.31, rent: 15},
+{name: "munich", long: 11.57, lat: 48.13, rent: 20},
+{name: "freiburg", long: 7.85, lat: 47.99, rent: 40}
 ]
-// Add circles:
-svg
-  .selectAll("myCircles")
-  .data(markers)
-  .join("circle")
-    .attr("id", "circleBasicTooltip")
-    .attr("cx", d => projection([d.long, d.lat])[0])
-    .attr("cy", d => projection([d.long, d.lat])[1])
-    .attr("r", 14)
-    .style("fill", "69b3a2")
-    .attr("stroke", "#69b3a2")
-    .attr("stroke-width", 3)
-    .attr("fill-opacity", .4)
+
+    // Bars
+let cityBars = svg.selectAll("mybar")
+.data(markers)
+.enter()
+.append("rect")
+  .attr("width", 10)
+  .attr("height", function(d) { return d.rent; })
+  .attr("x", function(d) { return projection([d.long, d.lat])[0] })
+  // y point pins the top left corner of the bar
+  // correct for the length of the bar in order to place at correct position of city
+  .attr("y", function(d) { return projection([d.long, d.lat])[1] - d.rent })
+  .attr("fill", "#69b3a2")
 
 
-// create a tooltip
-var tooltip = d3.select("#map")
-.append("div")
-.style("position", "absolute")
-.style("visibility", "hidden")
-.text("I'm a circle!");
-
-d3.select("#berlinToggle")
-.on("mousedown", function(){d3.select("#circleBasicTooltip").style("visibility", "hidden")})
-d3.select("#circleBasicTooltip")
-.on("mouseover", function(){return tooltip.style("visibility", "visible");})
-.on("mousemove", function(){return tooltip.style("top", (event.pageY)+"px").style("left",(event.pageX)+"px");})
-.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+let updateChart = function () {
+    
+    cityBars
+        .transition()
+        .duration(2000)
+        .attr("height", (d) =>  d.rent * 2)
+        .attr("y", function(d) { return projection([d.long, d.lat])[1] - 2 * d.rent })
+}
+document.getElementById("rentDecrease").onclick = updateChart;
 })
